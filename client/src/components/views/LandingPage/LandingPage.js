6fe2e4487ce2b4513f06b2f3de8c257a6e20b924
박지현 <pjh_5890@naver.com>
2020-06-22 13:35:16 +0900

    로그아웃 기능 완료, 로그인 되있을 경우에만 로그아웃 버튼 생성하게 변경하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => console.log(response.data))
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert("logout failed");
+                }
+            })
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
@@ -14,6 +25,9 @@ function LandingPage() {
         }}>
 
             <h2> start page </h2>
+            <button onClick={onClickHandler}>
+                logout
+            </button>
         </div>
 
     );
